74457cb91fc1dc7c79254eb12124bf915184a214
Spkspeed <spkspeed@gmail.com>
2022-03-29 18:15:32 -0300

    framework BULMA for CSS

--- src/app/components/formulario.component.ts/formulario.component.ts
+++ src/app/components/formulario.component.ts/formulario.component.ts
@@ -25,6 +25,7 @@ export class InicioFormulario implements OnInit {
             date: ['', [Validators.required]],
             email: ['', [Validators.required, Validators.email]],
             text: ['', [Validators.required, Validators.maxLength(80)]],
+            codigo: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
             category: ['', [Validators.required]],
             gender: ['', [Validators.required]],
         });
@@ -40,10 +41,37 @@ export class InicioFormulario implements OnInit {
             this.form.markAllAsTouched(),
         ]
     }
+    get emailField(){
+        return this.form.get('email');
+    }
+
+    get emailFieldIsValid(){
+        return this.emailField?.touched && this.emailField.valid;   
+    }
+
+    get emailFieldIsInvalid(){
+        return this.emailField?.touched && this.emailField.invalid;   
+    }
+
+    get codigoField(){
+        return this.form.get('codigo');
+    }
+
+    get codigoFieldIsValid(){
+        return this.codigoField?.touched && this.codigoField.valid;   
+    }
+
+    get codigoFieldIsInvalid(){
+        return this.codigoField?.touched && this.codigoField.invalid;   
+    }
+
+
 
     doSomething() {
         console.log('click');
     }
+
+
 
 }
 
